5f4980a87ebbd497c3fb71ca325139f4a1dfa1b9
Helge Stegemoen <helge@stegemoen.com>
2023-04-13 11:37:06 +0200

    Initial Project with Unit class

---
+++ UnitsOfMeasure/QuantityType.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitsOfMeasure
+{
+    internal class QuantityType
+    {
+        /* ToDo: Find out what the QuantityTypes are used for, and if I can delete it from this project
+         * Think the quantity types was used, because some Units had multiple quantity types. If so, it
+         * might be much work to create an insert script */
+
+        public int Id { get; }
+        public string Name { get; set; }
+
+        public QuantityType(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ UnitsOfMeasure/Unit.cs
@@ -0,0 +1,77 @@
+﻿using System.ComponentModel;
+
+namespace UnitsOfMeasure
+{
+    public class Units
+    {
+        // ToDo: Add base units to this class, since the same unit authorites can have different descriptions
+        public int UomId { get; set;  }
+        public string CatalogName { get; set; }
+        public string CatalogSymbol_isExplicit { get; set; }
+        public string CatalogSymbol_text { get; set; }
+        public string ConversionToBaseUnit_Factor { get; set; }
+        public string ConversionToBaseUnit_Formula_A { get; set; }
+        public string ConversionToBaseUnit_Formula_B { get; set; }
+        public string ConversionToBaseUnit_Formula_C { get; set; }
+        public string ConversionToBaseUnit_Formula_D { get; set; }
+        public string ConversionToBaseUnit_Fraction_Denominator { get; set; }
+        public string ConversionToBaseUnit_Fraction_Numerator { get; set; }
+        public string ConversionToBaseUnit_baseUnit { get; set; }
+        public string Deprecated { get; set; }
+        public string DimensionalClass { get; set; }
+        public string Name { get; set; }
+        public string Annotation { get; set; }
+        public string Id { get; set; }
+
+        public Units(
+            string catalogName, 
+            string catalogSymbol_isExplicit, 
+            string catalogSymbol_text, 
+            string conversionToBaseUnit_baseUnit,
+            string deprecated, 
+            string dimensionalClass, 
+            string name, 
+            string annotation,
+            string id)
+        {
+            CatalogName = catalogName;
+            CatalogSymbol_isExplicit = catalogSymbol_isExplicit;
+            CatalogSymbol_text = catalogSymbol_text;
+            ConversionToBaseUnit_baseUnit = conversionToBaseUnit_baseUnit;
+            Deprecated = deprecated;
+            DimensionalClass = dimensionalClass;
+            Name = name;
+            Annotation = annotation;
+            Id = id;
+        }
+
+        public Units(
+            string catalogName, 
+            string catalogSymbol_isExplicit, 
+            string catalogSymbol_text, 
+            string conversionToBaseUnit_Factor, 
+            string conversionToBaseUnit_Formula_A, 
+            string conversionToBaseUnit_Formula_B, 
+            string conversionToBaseUnit_Formula_C, 
+            string conversionToBaseUnit_Formula_D, 
+            string conversionToBaseUnit_Fraction_Denominator, 
+            string conversionToBaseUnit_Fraction_Numerator, 
+            string conversionToBaseUnit_baseUnit, 
+            string deprecated, string dimensionalClass, 
+            string name, 
+            string annotation, 
+            string id) : 
+            this(catalogName, catalogSymbol_isExplicit, catalogSymbol_text, conversionToBaseUnit_Factor, 
+                conversionToBaseUnit_Formula_A, conversionToBaseUnit_Formula_B, conversionToBaseUnit_Formula_C, 
+                conversionToBaseUnit_Formula_D, conversionToBaseUnit_Fraction_Denominator)
+        {
+            ConversionToBaseUnit_Fraction_Numerator = conversionToBaseUnit_Fraction_Numerator;
+            ConversionToBaseUnit_baseUnit = conversionToBaseUnit_baseUnit;
+            Deprecated = deprecated;
+            DimensionalClass = dimensionalClass;
+            Name = name;
+            Annotation = annotation;
+            Id = id;
+        }
+    }
+}
