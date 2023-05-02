29f1e04fd3f1d7b80fb90944341e27d50dd6881b
Helge Stegemoen <helge@stegemoen.com>
2023-05-02 20:54:33 +0200

    Added project that uses the new UoM database

---
+++ UoM2023_ConsoleApp/DBScripts/InsertInto_SameUnitAndQuantityType_tables.sql
@@ -0,0 +1,105 @@
+﻿CREATE TABLE SameUnit (
+    SameUnitId int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+    SameUnit_uom varchar(50),
+	SameUnit_namingSystem varchar(50),
+    UomId int FOREIGN KEY REFERENCES UnitsOfMeasure(UomId)
+); 
+
+/* Insert SameUnit 1-3 */
+INSERT INTO SameUnit (UomId, SameUnit_uom, SameUnit_namingSystem)
+SELECT UnitsOfMeasure.UomId, UnitsOfMeasure.SameUnit_3__uom, UnitsOfMeasure.SameUnit_3__namingSystem
+FROM UnitsOfMeasure
+WHERE SameUnit_3__uom IS NOT NULL
+
+INSERT INTO SameUnit (UomId, SameUnit_uom, SameUnit_namingSystem)
+SELECT UnitsOfMeasure.UomId, UnitsOfMeasure.SameUnit_2__uom, UnitsOfMeasure.SameUnit_2__namingSystem
+FROM UnitsOfMeasure
+WHERE SameUnit_2__uom IS NOT NULL /* Check that SameUnit_2 for UomID 341 is added */
+
+INSERT INTO SameUnit (UomId, SameUnit_uom, SameUnit_namingSystem)
+SELECT UnitsOfMeasure.UomId, UnitsOfMeasure.SameUnit_1__uom, UnitsOfMeasure.SameUnit_1__namingSystem
+FROM UnitsOfMeasure
+WHERE SameUnit_1__uom IS NOT NULL /* Check that SameUnit_2 for UomID 33 and 439 is added */
+
+INSERT INTO SameUnit (UomId, SameUnit_uom, SameUnit_namingSystem)
+SELECT UnitsOfMeasure.UomId, UnitsOfMeasure.SameUnit_0__uom, UnitsOfMeasure.SameUnit_0__namingSystem
+FROM UnitsOfMeasure
+WHERE SameUnit_0__uom IS NOT NULL /* Check that SameUnit_2 for UomID 437 and 438 is added */
+
+
+/* Create QuantityType table */
+CREATE TABLE QuantityType (
+    QuantityTypeId int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+    QuantityType varchar(200),
+    UomId int FOREIGN KEY REFERENCES UnitsOfMeasure(UomId)
+); 
+/* Insert QuantityType 0-12 */
+INSERT INTO QuantityType (UomId, QuantityType)
+SELECT UnitsOfMeasure.UomId, UnitsOfMeasure.QuantityType_0
+FROM UnitsOfMeasure
+WHERE QuantityType_0 IS NOT NULL
+
+INSERT INTO QuantityType (UomId, QuantityType)
+SELECT UnitsOfMeasure.UomId, UnitsOfMeasure.QuantityType_1
+FROM UnitsOfMeasure
+WHERE QuantityType_1 IS NOT NULL
+
+INSERT INTO QuantityType (UomId, QuantityType)
+SELECT UnitsOfMeasure.UomId, UnitsOfMeasure.QuantityType_2
+FROM UnitsOfMeasure
+WHERE QuantityType_2 IS NOT NULL
+
+INSERT INTO QuantityType (UomId, QuantityType)
+SELECT UnitsOfMeasure.UomId, UnitsOfMeasure.QuantityType_3
+FROM UnitsOfMeasure
+WHERE QuantityType_3 IS NOT NULL
+
+INSERT INTO QuantityType (UomId, QuantityType)
+SELECT UnitsOfMeasure.UomId, UnitsOfMeasure.QuantityType_4
+FROM UnitsOfMeasure
+WHERE QuantityType_4 IS NOT NULL
+
+INSERT INTO QuantityType (UomId, QuantityType)
+SELECT UnitsOfMeasure.UomId, UnitsOfMeasure.QuantityType_5
+FROM UnitsOfMeasure
+WHERE QuantityType_5 IS NOT NULL
+
+INSERT INTO QuantityType (UomId, QuantityType)
+SELECT UnitsOfMeasure.UomId, UnitsOfMeasure.QuantityType_6
+FROM UnitsOfMeasure
+WHERE QuantityType_6 IS NOT NULL
+
+INSERT INTO QuantityType (UomId, QuantityType)
+SELECT UnitsOfMeasure.UomId, UnitsOfMeasure.QuantityType_7
+FROM UnitsOfMeasure
+WHERE QuantityType_7 IS NOT NULL
+
+INSERT INTO QuantityType (UomId, QuantityType)
+SELECT UnitsOfMeasure.UomId, UnitsOfMeasure.QuantityType_8
+FROM UnitsOfMeasure
+WHERE QuantityType_8 IS NOT NULL
+
+INSERT INTO QuantityType (UomId, QuantityType)
+SELECT UnitsOfMeasure.UomId, UnitsOfMeasure.QuantityType_9
+FROM UnitsOfMeasure
+WHERE QuantityType_9 IS NOT NULL
+
+INSERT INTO QuantityType (UomId, QuantityType)
+SELECT UnitsOfMeasure.UomId, UnitsOfMeasure.QuantityType_10
+FROM UnitsOfMeasure
+WHERE QuantityType_10 IS NOT NULL
+
+INSERT INTO QuantityType (UomId, QuantityType)
+SELECT UnitsOfMeasure.UomId, UnitsOfMeasure.QuantityType_11
+FROM UnitsOfMeasure
+WHERE QuantityType_11 IS NOT NULL
+
+INSERT INTO QuantityType (UomId, QuantityType)
+SELECT UnitsOfMeasure.UomId, UnitsOfMeasure.QuantityType_12
+FROM UnitsOfMeasure
+WHERE QuantityType_12 IS NOT NULL
+
+/* One Unit has no QuantityType */
+  /* 1089 has no QuantityType_0, so 'SELECT DISTINCT UomId FROM QuantityType' shows only 1289 units, which is not an error */
+  SELECT * FROM UnitsOfMeasure WHERE UomId = 1089 // QuantityType is also null in http://w3.energistics.org/uom/poscUnits22.xml
+
---
+++ Uom2023/Models/ConversionToBaseUnit.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Uom2023.Models;
+
+[Table("ConversionToBaseUnit")]
+public partial class ConversionToBaseUnit
+{
+    [Key]
+    public int ConversionId { get; set; }
+
+    [Column("ConversionToBaseUnit_Factor")]
+    [StringLength(100)]
+    [Unicode(false)]
+    public string? ConversionToBaseUnitFactor { get; set; }
+
+    [Column("ConversionToBaseUnit__baseUnit")]
+    [StringLength(100)]
+    [Unicode(false)]
+    public string? ConversionToBaseUnitBaseUnit { get; set; }
+
+    [Column("ConversionToBaseUnit_Fraction_Numerator")]
+    [StringLength(100)]
+    [Unicode(false)]
+    public string? ConversionToBaseUnitFractionNumerator { get; set; }
+
+    [Column("ConversionToBaseUnit_Fraction_Denominator")]
+    [StringLength(100)]
+    [Unicode(false)]
+    public string? ConversionToBaseUnitFractionDenominator { get; set; }
+
+    [Column("ConversionToBaseUnit_Formula_A")]
+    [StringLength(50)]
+    [Unicode(false)]
+    public string? ConversionToBaseUnitFormulaA { get; set; }
+
+    [Column("ConversionToBaseUnit_Formula_B")]
+    [StringLength(50)]
+    [Unicode(false)]
+    public string? ConversionToBaseUnitFormulaB { get; set; }
+
+    [Column("ConversionToBaseUnit_Formula_C")]
+    [StringLength(50)]
+    [Unicode(false)]
+    public string? ConversionToBaseUnitFormulaC { get; set; }
+
+    [Column("ConversionToBaseUnit_Formula_D")]
+    [StringLength(50)]
+    [Unicode(false)]
+    public string? ConversionToBaseUnitFormulaD { get; set; }
+
+    public int? UomId { get; set; }
+
+    [ForeignKey("UomId")]
+    [InverseProperty("ConversionToBaseUnits")]
+    public virtual UnitsOfMeasure? Uom { get; set; }
+}
