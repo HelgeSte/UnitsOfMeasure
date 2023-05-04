b30a8a66424e18283fe8a8ac0db086962e8e4848
Helge Stegemoen <helge@stegemoen.com>
2023-05-04 19:43:55 +0200

    Done some testing with sql connections for class library

--- UomLibrary/Data/Uom2023Context.cs
+++ UomLibrary/Data/Uom2023Context.cs
@@ -25,7 +25,6 @@ public partial class Uom2023Context : DbContext
     public virtual DbSet<UnitsOfMeasure> UnitsOfMeasures { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("Data Source=DESKTOP-LN5BF6J;Initial Catalog=uom2023;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
